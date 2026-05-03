e6c3b96091b71a9b20af36cc4af6674bfa140d5f
Anurag Kumar Upadhyay <akupadhaya01@gmail.com>
2026-05-03 19:06:43 +0530

    Delete frontend directory

--- frontend/supabaseClient.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-// These will be your unique project credentials
-const supabaseUrl = 'YOUR_SUPABASE_URL';
-const supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
